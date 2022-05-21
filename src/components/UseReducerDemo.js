44280f4e032ccd2b7af25b6f3e25307bd277552f
Tawsif Haque <tawsifmasrur@gmail.com>
2022-05-21 11:20:58 +0600

    ...

--- src/components/UseReducerDemo.js
+++ src/components/UseReducerDemo.js
@@ -8,22 +8,25 @@ const reducer = (state, action) => {
     case "decrement":
       return { ...state, count: state.count - 1 };
 
+    case "setUserInput":
+      return { ...state, userInput: action.payload };
+
     default:
       throw new Error();
   }
 };
 
 const useReducerDemo = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  const [userInput, setUserInput] = useState("");
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0, userInput: "" });
 
   return (
     <div>
       <input
         type="text"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
+        value={state.userInput}
+        onChange={(e) =>
+          dispatch({ type: "setUserInput", payload: e.target.value })
+        }
       />
 
       <p>{state.count}</p>
@@ -34,7 +37,7 @@ const useReducerDemo = () => {
         <button onClick={() => dispatch({ type: "decrement" })}>-</button>
       </section>
 
-      <p>userInput: {userInput}</p>
+      <p>userInput: {state.userInput}</p>
     </div>
   );
 };
