e704aac30051f13c023467a4071dfb07568c0af8
Iftee1711402 <tawsifhaque97@gmail.com>
2022-06-06 05:10:05 +0600

    ...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-// import GetPosts from "./components/GetPosts";
-// import GetPosts2 from "./components/GetPosts2";
-// import UseStateDemo from "./components/UseStateDemo";
+import GetPosts from "./components/GetPosts";
+import GetPosts2 from "./components/GetPosts2";
+import UseStateDemo from "./components/UseStateDemo";
 import UseReducerDemo from "./components/UseReducerDemo";
 
 function App() {
-  return (
-    <div>
-      {/* <GetPosts /> */}
-      {/* <GetPosts2 /> */}
-      {/* <UseStateDemo /> */}
-      <UseReducerDemo />
-    </div>
-  );
+    return (
+        <div>
+            {/* <GetPosts /> */}
+            {/* <GetPosts2 /> */}
+            {/* <UseStateDemo /> */}
+            <UseReducerDemo />
+        </div>
+    );
 }
 
 export default App;
--- src/components/UseReducerDemo.js
+++ src/components/UseReducerDemo.js
@@ -1,45 +1,44 @@
 import React, { useReducer } from "react";
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { ...state, count: state.count + 1 };
+    switch (action.type) {
+        case "increment":
+            return { ...state, count: state.count + 1 };
 
-    case "decrement":
-      return { ...state, count: state.count - 1 };
+        case "decrement":
+            return { ...state, count: state.count - 1 };
 
-    case "setUserInput":
-      return { ...state, userInput: action.payload };
+        case "setUserInput":
+            return { ...state, userInput: action.payload };
 
-    default:
-      throw new Error();
-  }
+        default:
+            throw new Error();
+    }
 };
 
 const useReducerDemo = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0, userInput: "" });
+    const [state, dispatch] = useReducer(reducer, { count: 0, userInput: "" });
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={state.userInput}
-        onChange={(e) =>
-          dispatch({ type: "setUserInput", payload: e.target.value })
-        }
-      />
+    return (
+        <div>
+            <input
+                type="text"
+                value={state.userInput}
+                onChange={(e) =>
+                    dispatch({ type: "setUserInput", payload: e.target.value })
+                }
+            />
 
-      <p>{state.count}</p>
+            <p>{state.count}</p>
 
-      <section>
-        <button onClick={() => dispatch({ type: "increment" })}>+</button>
+            <section>
+                <button onClick={() => dispatch({ type: "increment" })}>+</button>
+                <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+            </section>
 
-        <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      </section>
-
-      <p>userInput: {state.userInput}</p>
-    </div>
-  );
+            <p>userInput: {state.userInput}</p>
+        </div>
+    );
 };
 
 export default useReducerDemo;
